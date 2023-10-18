6d5d90a7c9657e3d3df275390c46dd848360728e
AhmedElshenawy0 <shenawy100@gmail.com>
2023-10-19 00:00:29 +0200

    firstt

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "dist",
   images: {
-    domains: ["images.pexels.com", "miro.medium.com", "www.cnet.com"],
+    unoptimized: true,
   },
 };
 
