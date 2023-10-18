65ff9d5ab20f43a25a140ed1229992f9ae406b4b
AhmedElshenawy0 <shenawy100@gmail.com>
2023-10-18 23:07:27 +0200

    firstt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+  images: {
+    domains: ["images.pexels.com", "miro.medium.com", "www.cnet.com"],
+  },
 };
 
 module.exports = nextConfig;
